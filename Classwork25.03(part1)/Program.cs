6b1845791fb710fd67c4f39bd28a5993f1a8a2cb
mariam <mariam@MYPROBOOK>
2023-03-25 16:57:51 +0400

    Add project files.

---
+++ Classwork25.03(part1)/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Classwork25._03_part1_
+{
+    internal class Program
+    {
+            enum name
+            {
+            hey,
+            bye, 
+            no,
+            yes
+            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(((name)2).GetType());
+            var s = ((name)2);
+            Console.WriteLine(s.ToString().Length.GetTypeCode());
+            s = s.ToString().Length;
+            Console.WriteLine(s);
+            Random random = new Random();
+            int num = random.Next(100);
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = num++;
+                Console.WriteLine(arr[i]);
+            }           
+
+        }
+    }
+}
---
+++ Classwork25.03DataStructures/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Classwork25._03DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[101];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+                        
+            int target = 24;            
+
+            int left = 0; 
+            int right = numbers.Length - 1; 
+            int count = 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2; 
+
+                if (numbers[mid] == target)
+                {
+                    Console.WriteLine("The number {0} is at index {1}", target, mid);
+                    Console.WriteLine(count);
+                    break;
+                }
+                else if (numbers[mid] < target)
+                {
+                    left = mid + 1; 
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+                count++;
+            }       
+
+        }
+    }
+}
